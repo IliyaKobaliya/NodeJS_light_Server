ebde2bf5ee1e283f5788f50e78e411078d0f9831
IliyaKobaliya <43680599+IliyaKobaliya@users.noreply.github.com>
2019-01-14 10:11:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (req,res) {
     console.log(req.url);
-    res.writeHead(200,{'Content-Type':'text/plaine; charset = utd-8'});
+    res.writeHead(200,{'Content-Type':'text/plaine; charset = utf-8'});
     res.end('Oh no Oh no');
 });
 server.listen(3000,'127.0.0.1');
